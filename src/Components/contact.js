d139d6c1b75e495c8f04d720ef22f34c8990994d
ThkPsss <christanspence66@gmail.com>
2020-03-24 21:21:57 -0400

    Contact page finished

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 
 class Contact extends Component {
     render() {
         return(
-            <div><hi>Contact Page</hi></div>
+            <div className='contact-body'>
+                <Grid className='contact-grid'>
+                    <Cell col={6}>
+                        <h2>Christan Spence</h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt='avatar'
+                            style={{height: '250px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Junior Web Developer specializing in backend development I am knowledgeable in C#, JavaScript and other back-end related software. I have experience in front end work like CSS and Adobe XD. I am eager to learn more and grow my experience in the field. </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div className='contact-list'>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className='fa fa-phone-square' aria-hidden='true' />
+                                        (631)839-3291
+                                        </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className='fa fa-envelope' aria-hidden='true' />
+                                        ChristanSpence66@gmail.com
+                                        </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
